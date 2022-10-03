592d78c4d64c8121724086a2af50841332feba8a
Christian Ramos <christianramoslopez10@gmail.com>
2022-10-03 17:15:03 -0300

    Course Update
    -Finished Sections 13, 14.
    -Finished Simon Game Challenge

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,29 @@
+var counter = 0;
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    switch (this.innerHTML) {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key){
+  if(counter == 5){
+    var audio = new Audio('sounds/rickRolled.mp3');
+    if(!playingAudio(audio)){
+      audio.play();
+    }
+    else{
+      return;
+    }
+  }
+  else if(counter < 5)
+    switch (key) {
       case "w":
         var audio = new Audio('sounds/tom-1.mp3');
         audio.play();
@@ -33,41 +55,19 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
       default:
         break;
     }
-  });
+    counter++;
 }
 
-document.addEventListener("keydown", function(event){
-  var key = event.key;
-  switch(key){
-    case "w":
-      var audio = new Audio('sounds/tom-1.mp3');
-      audio.play();
-      break;
-    case "a":
-      var audio = new Audio('sounds/tom-2.mp3');
-      audio.play();
-      break;
-    case "s":
-      var audio = new Audio('sounds/tom-3.mp3');
-      audio.play();
-      break;
-    case "d":
-      var audio = new Audio('sounds/tom-4.mp3');
-      audio.play();
-      break;
-    case "j":
-      var audio = new Audio('sounds/snare.mp3');
-      audio.play();
-      break;
-    case "k":
-      var audio = new Audio('sounds/kick-bass.mp3');
-      audio.play();
-      break;
-    case "l":
-      var audio = new Audio('sounds/crash.mp3');
-      audio.play();
-      break;
-    default:
-      break;
-  }
-});
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    //Un-style after .1s
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+function playingAudio(audio){
+  return !audio.paused;
+}
